fb8a7f57d2254f764125b1976dd660ec0bf6ba0d
Werner Rauch <werner.rauch@interone.de>
2026-09-14 17:13:13 +0200

    update tsConfig

---
+++ src/utility/utility.ts
@@ -0,0 +1,5 @@
+import CommandLoader from './CommandLoader';
+import CharacterManager from './chracterLoader';
+import EventUtility from './eventUtility';
+
+export { CommandLoader, CharacterManager, EventUtility };
